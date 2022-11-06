bbab4b3394a6e5a77b4e2020440652b10295c273
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-07 02:29:48 +0600

    Models Added

---
+++ EmployeeAttendance/EmployeeAttendance.Web/Areas/Admin/Models/AttendanceCreateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAttendance.Web.Areas.Admin.Models
+{
+    public class AttendanceCreateModel
+    {
+        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public TimeOnly InTime { get; set; }
+        public TimeOnly OutTime { get; set; }
+        public string Remarks { get; set; }
+    }
+}
---
+++ EmployeeAttendance/EmployeeAttendance.Web/Areas/Admin/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+
+namespace EmployeeAttendance.Web.Areas.Admin.Models
+{
+    public class EmployeeModel
+    {
+        internal void ResolveDependency(ILifetimeScope scope)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object GetAllEmployee()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
