d1a9b2bf613465adbf8fb2cccc33936deba37a04
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-07 02:30:17 +0600

    AttendanceController Initialized

---
+++ EmployeeAttendance/EmployeeAttendance.Web/Areas/Admin/Controllers/AttendanceController.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using EmployeeAttendance.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAttendance.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AttendanceController : Controller
+    {
+        private readonly ILifetimeScope _scope;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        //[HttpGet]
+        //public ActionResult GetEmployeeProfileData()
+        //{
+        //    EmployeeModel employeeModel = _scope.Resolve<EmployeeModel>();
+        //    employeeModel.ResolveDependency(_scope);
+
+        //    var data = employeeModel.GetAllEmployee();
+        //    return Json(data);
+        //}
+    }
+}
