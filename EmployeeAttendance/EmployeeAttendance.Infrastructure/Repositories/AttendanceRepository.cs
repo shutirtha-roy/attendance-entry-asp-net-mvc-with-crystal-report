3443e433960a74c82560b0c8c36b3fa018a1b0c7
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-07 19:31:28 +0600

    AttendanceRepositoryAdded

---
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/Repositories/AttendanceRepository.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeAttendance.Infrastructure.DbContext;
+using EmployeeAttendance.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance.Infrastructure.Repositories
+{
+    public class AttendanceRepository : Repository<AttendanceEntity>, IAttendanceRepository
+    {
+        private TrainingDbContext _db;
+
+        public AttendanceRepository(TrainingDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(AttendanceEntity obj)
+        {
+            _db.Attendances.Update(obj);
+        }
+    }
+}
---
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/Repositories/IAttendanceRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeAttendance.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance.Infrastructure.Repositories
+{
+    public interface IAttendanceRepository : IRepository<AttendanceEntity>
+    {
+        void Update(AttendanceEntity obj);
+    }
+}
