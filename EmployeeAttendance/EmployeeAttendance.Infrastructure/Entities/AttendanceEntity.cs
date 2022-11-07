924086fb9e4d9ba1471b2561e98f3bf91befe906
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-07 19:26:32 +0600

    Entities Implemented

---
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/Entities/AttendanceEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance.Infrastructure.Entities
+{
+    public class AttendanceEntity : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public Guid EmployeeId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime InTime { get; set; }
+        public DateTime OutTime { get; set; }
+        public string Remarks { get; set; }
+    }
+}
