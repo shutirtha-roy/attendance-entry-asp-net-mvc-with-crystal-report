0ece1cfe7086d12c5276ddb1652282869d9021e9
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-07 19:27:30 +0600

    Base Repository Added

---
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance.Infrastructure.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
---
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using EmployeeAttendance.Infrastructure.DbContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly TrainingDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(TrainingDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll(string? includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
