fb0b90008357aee509200ed05383578319d2ffb5
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-09 03:46:47 +0600

    AttendanceCreateModel Implemented

--- EmployeeAttendance/EmployeeAttendance.Web/Areas/Admin/Models/AttendanceCreateModel.cs
+++ EmployeeAttendance/EmployeeAttendance.Web/Areas/Admin/Models/AttendanceCreateModel.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Autofac;
+using EmployeeAttendance.Infrastructure.BusinessObjects;
+using EmployeeAttendance.Infrastructure.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace EmployeeAttendance.Web.Areas.Admin.Models
 {
@@ -15,12 +18,37 @@ namespace EmployeeAttendance.Web.Areas.Admin.Models
         [Required]
         public DateTime OutTime { get; set; }
         public string Remarks { get; set; }
+        private ILifetimeScope _scope;
+        private IAttendanceService _attendanceService;
 
         public AttendanceCreateModel()
         {
 
         }
 
+        public AttendanceCreateModel(IAttendanceService attendanceService)
+        {
+            _attendanceService = attendanceService;
+        }
+
+        public void ResolveDependency(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _attendanceService = _scope.Resolve<IAttendanceService>();
+        }
+
+        internal void CreateAttendance()
+        {
+            Attendance attendance = new Attendance();
+
+            attendance.EmployeeId = EmployeeId;
+            attendance.CreatedDate = CreatedDate;
+            attendance.InTime = InTime;
+            attendance.OutTime = OutTime;
+            attendance.Remarks = Remarks;
+
+            _attendanceService.CreateAttendance(attendance);
+        }
 
     }
 }
