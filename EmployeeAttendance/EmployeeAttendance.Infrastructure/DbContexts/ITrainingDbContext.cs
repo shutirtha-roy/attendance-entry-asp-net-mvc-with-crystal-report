fb5820f69e7c36dc8bc0453e9adf9e942631c4c4
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-09 03:47:58 +0600

    TrainingDbContext Added

---
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/DbContexts/ITrainingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeAttendance.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance.Infrastructure.DbContexts
+{
+    public interface ITrainingDbContext
+    {
+        public DbSet<AttendanceEntity> Attendances { get; set; }
+    }
+}
---
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/DbContexts/TrainingDbContext.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeAttendance.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAttendance.Infrastructure.DbContexts
+{
+    public class TrainingDbContext : IdentityDbContext, ITrainingDbContext
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+
+        public TrainingDbContext(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString,
+                    b => b.MigrationsAssembly(_migrationAssemblyName)
+                );
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<AttendanceEntity> Attendances { get; set; }
+    }
+}
