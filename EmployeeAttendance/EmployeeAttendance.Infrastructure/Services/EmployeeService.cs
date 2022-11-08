d1b70b0bea342dcfcbbd06494539484dd35b15c0
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-09 03:36:17 +0600

    EmployeeService Implemented

--- EmployeeAttendance/EmployeeAttendance.Infrastructure/Services/EmployeeService.cs
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/Services/EmployeeService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +12,43 @@ namespace EmployeeAttendance.Infrastructure.Services
 {
     public class EmployeeService : IEmployeeService
     {
-        public void GetAllEmployee()
+        private readonly string _connectionString;
+
+        public EmployeeService(IConfiguration config)
         {
-            throw new NotImplementedException();
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        private SqlCommand PrepareCommand(string sqlCommand)
+        {
+            SqlConnection sqlConnection = new SqlConnection(_connectionString);
+            SqlCommand command = new SqlCommand(sqlCommand, sqlConnection);
+
+            return command;
+        }
+
+        public dynamic GetAllEmployee()
+        {
+            string command = "Select * from Hrms_Company_Employee_Profile";
+            using SqlCommand sqlCommand = PrepareCommand(command);
+
+            if (sqlCommand.Connection.State != System.Data.ConnectionState.Open)
+                sqlCommand.Connection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataSet ds = new DataSet();
+            sqlDataAdapter.Fill(ds);
+
+            List<SelectListItem> employeeProfileList = new List<SelectListItem>();
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                employeeProfileList.Add(new SelectListItem { Text = dr["EmployeeName"].ToString(), Value = dr["EmployeeProfileId"].ToString() }); ;
+            }
+
+            var data = from value in employeeProfileList select new { value.Text, value.Value };
+
+            return data;
         }
     }
 }
--- EmployeeAttendance/EmployeeAttendance.Infrastructure/Services/IEmployeeService.cs
+++ EmployeeAttendance/EmployeeAttendance.Infrastructure/Services/IEmployeeService.cs
@@ -8,6 +8,6 @@ namespace EmployeeAttendance.Infrastructure.Services
 {
     public interface IEmployeeService
     {
-        void GetAllEmployee();
+        dynamic GetAllEmployee();
     }
 }
