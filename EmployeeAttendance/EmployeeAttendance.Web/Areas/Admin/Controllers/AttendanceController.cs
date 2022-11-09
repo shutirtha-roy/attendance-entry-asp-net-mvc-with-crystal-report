b203affaf46d00178458059d641476b5dd2fa312
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-09 17:12:04 +0600

    Create GET Implemented

--- EmployeeAttendance/EmployeeAttendance.Web/Areas/Admin/Controllers/AttendanceController.cs
+++ EmployeeAttendance/EmployeeAttendance.Web/Areas/Admin/Controllers/AttendanceController.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using EmployeeAttendance.Web.Areas.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EmployeeAttendance.Web.Areas.Admin.Controllers
 {
@@ -9,21 +10,36 @@ namespace EmployeeAttendance.Web.Areas.Admin.Controllers
     {
         private readonly ILifetimeScope _scope;
 
+        public AttendanceController(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
+        public IActionResult Create()
+        {
+            AttendanceViewModel attendanceVM = _scope.Resolve<AttendanceViewModel>();
+            attendanceVM.Attendance = _scope.Resolve<AttendanceCreateModel>();
+            attendanceVM.EmployeeList = GetEmployeeProfileData();
+
+            return View(attendanceVM);
+        }
 
 
-        //[HttpGet]
-        //public ActionResult GetEmployeeProfileData()
-        //{
-        //    EmployeeModel employeeModel = _scope.Resolve<EmployeeModel>();
-        //    employeeModel.ResolveDependency(_scope);
+        [HttpGet]
+        public dynamic GetEmployeeProfileData()
+        {
+            EmployeeModel employeeModel = _scope.Resolve<EmployeeModel>();
+            employeeModel.ResolveDependency(_scope);
 
-        //    var data = employeeModel.GetAllEmployee();
-        //    return Json(data);
-        //}
+            var data = employeeModel.GetAllEmployeeProfile();
+
+            return data;
+        }
+
     }
 }
