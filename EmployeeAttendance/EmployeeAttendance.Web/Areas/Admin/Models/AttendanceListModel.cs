17771c0ff0a74c9a6313228120f537944fc19ded
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-11-10 18:18:42 +0600

    AttendanceListModel Implemented

---
+++ EmployeeAttendance/EmployeeAttendance.Web/Areas/Admin/Models/AttendanceListModel.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using EmployeeAttendance.Infrastructure.Services;
+
+namespace EmployeeAttendance.Web.Areas.Admin.Models
+{
+    public class AttendanceListModel
+    {
+        private ILifetimeScope _scope;
+        private IAttendanceService? _attendanceService;
+
+        public AttendanceListModel(ILifetimeScope scope)
+        {
+            _scope = scope;
+        }
+
+        public void ResolveDependency(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _attendanceService = _scope.Resolve<IAttendanceService>();
+        }
+
+        internal object? GetAllAttendance()
+        {
+            return _attendanceService?.GetAllAtttendance();
+        }
+    }
+}
